725af6190d6a0ae7efa80c24c3e4db4f04ed7b07
Hayden-J <1761373255@qq.com>
2022-04-30 15:32:27 +0800

    compress img

--- lib/chat_content/send_image_btn.dart
+++ lib/chat_content/send_image_btn.dart
@@ -1,12 +1,12 @@
 /// chat_box - send_image_btn
 /// Created by xhz on 29/04/2022
 import 'dart:developer';
-
 import 'package:chat_box/utils/toast.dart';
 import 'package:flutter/material.dart';
 import '../global.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class SendImageButton extends StatelessWidget {
   const SendImageButton({Key? key}) : super(key: key);
@@ -18,14 +18,24 @@ class SendImageButton extends StatelessWidget {
       toast("Something went wrong");
       return;
     }
-
+    toast(picker.path);
+    var result = await FlutterImageCompress.compressWithFile(
+      picker.path,
+      minWidth: 1200,
+      minHeight: 1200,
+      quality: 88,
+    );
+    if (null == result) {
+      toast("Compress Image went wrong");
+      return;
+    }
     final res = await http.post(Uri.parse(Global.https + Global.imageHost),
-        headers: {'content-type': 'image/' + picker.name.split('.').last}, body: await picker.readAsBytes());
+        headers: {'content-type': 'image/' + picker.name.split('.').last}, body: result);
     if (res.statusCode == 200) {
       //body 是储存file的名字
       Global.chatMessagesController.send(res.body, true);
     } else {
-      toast(res.body);
+      toast('Error code: ${res.statusCode}');
     }
   }
 
